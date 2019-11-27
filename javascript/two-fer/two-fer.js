f3afd145f2a3c7cd32212d3adb8d4f6ab981856a
Thanh Tung <7121553+tunggnu@users.noreply.github.com>
2019-11-27 14:31:27 +0700

    [JavaScript] 02. Two Fer

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = name => {
+  const displayName = name ? name : 'you';
+  
+  return `One for ${displayName}, one for me.`;
+};
