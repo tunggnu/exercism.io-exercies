1c94e7522695420391f91a063afc3f4c386dcaa3
Thanh Tung <7121553+tunggnu@users.noreply.github.com>
2019-11-29 09:58:25 +0700

    [JavaScript] 04. Resistor Color Duo
    Use `import` to reuse code from previous exercise.

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,15 +1,4 @@
-const COLORS = [
-  "black",
-  "brown",
-  "red",
-  "orange",
-  "yellow",
-  "green",
-  "blue",
-  "violet",
-  "grey",
-  "white"
-];
+import {COLORS} from '../resistor-color/resistor-color';
 
 export const value = ([firstColor, secondColor]) => (
   COLORS.indexOf(firstColor) * 10 + COLORS.indexOf(secondColor)
