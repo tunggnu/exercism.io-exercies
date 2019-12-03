93d9242a6d25f472155b4a02fe0f64c7f1e41c41
Thanh Tung <7121553+tunggnu@users.noreply.github.com>
2019-12-03 09:34:57 +0700

    JavaScript] Extra.Easy.15 Perfect Numbers
    Improve the version 1: Use a sum variable instead of using an array to store factors.

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -1,27 +1,25 @@
 export const classify = (number) => {
   number = Number(number);
 
-  if (!number || number < 1)
+  if (!number || number < 1) {
     throw new Error('Classification is only possible for natural numbers.');
+  }
 
   if (number === 1) return 'deficient';
 
-  const factors = [];
   const sqrt = Math.floor(Math.sqrt(number));
+  let aliquotSum = 0;
 
   for (let i = 1; i <= sqrt; i++) {
     if (number % i === 0) {
-      factors.push(i);
-      factors.push(number / i);
+      aliquotSum += i + number / i;
     }
   }
 
-  const aliquotSum =
-    factors.reduce((sum, factor) => sum + factor, 0) -
-    number -
-    (sqrt ** 2 === number ? sqrt : 0);
+  // Exclude the number itself and it's square root (if it is a square number).
+  aliquotSum -= number + (sqrt ** 2 === number ? sqrt : 0);
 
   if (aliquotSum === number) return 'perfect';
   if (aliquotSum < number) return 'deficient';
-  if (aliquotSum > number) return 'abundant';
+  return 'abundant';
 };
